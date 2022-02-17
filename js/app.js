9730491c04e3f0c285c7f47e7f1b2288ba6665d9
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-17 14:42:38 +0600

    Balance Funtionality Complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-console.log("Connected Money Calculator");
+// Calculate Total Cost:
+document.getElementById('calculate-btn').addEventListener('click', function () {
+        // Call totalIncome Function to get Total Income:
+        const totalIncome = getIncome();
+
+        // Call getTotalExpenses Function to get Total Expenses:
+        const totalExpenses = getTotalExpenses();
+
+        // Update Balance:
+        const updateBalance = totalIncome - totalExpenses;
+        document.getElementById('balance').innerText = updateBalance;
+
+})
+
+// Get Total Income:
+function getIncome() {
+        // Get Income Value:
+        const incomeInput = document.getElementById('income').value;
+        const income = parseFloat(incomeInput);
+        return income;
+}
+
+// Get Total Expenses:
+function getTotalExpenses() {
+        const foodInput = document.getElementById('food').value;
+        const food = parseFloat(foodInput);
+
+        const rentInput = document.getElementById('rent').value;
+        const rent = parseFloat(rentInput);
+
+        const clothesInput = document.getElementById('clothes').value;
+        const clothes = parseFloat(clothesInput);
+
+        // Added All Expenses:
+        const totalExpenses = food + rent + clothes;
+
+        // Update Total Expenses Inner Text:
+        document.getElementById('expenses').innerText = totalExpenses;
+        return totalExpenses;
+}
